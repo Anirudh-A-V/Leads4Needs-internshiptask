8ead6d096e0eda5b922299ad8ae31d7a65f1a59e
Anirudh A V <anirudh.av02@gmail.com>
2022-11-10 10:42:29 +0530

    unnecessary methods removed

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -33,43 +33,43 @@ router.get('/getAll', async (req, res) => {
     }
 })
 
-//Get by ID Method
-router.get('/getOne/:id', async (req, res) => {
-    try{
-        const data = await Model.findById(req.params.id);
-        res.json(data)
-    }
-    catch(error){
-        res.status(500).json({message: error.message})
-    }
-})
+// //Get by ID Method
+// router.get('/getOne/:id', async (req, res) => {
+//     try{
+//         const data = await Model.findById(req.params.id);
+//         res.json(data)
+//     }
+//     catch(error){
+//         res.status(500).json({message: error.message})
+//     }
+// })
 
-//Update by ID Method
-router.patch('/update/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const updatedData = req.body;
-        const options = { new: true };
+// //Update by ID Method
+// router.patch('/update/:id', async (req, res) => {
+//     try {
+//         const id = req.params.id;
+//         const updatedData = req.body;
+//         const options = { new: true };
 
-        const result = await Model.findByIdAndUpdate(
-            id, updatedData, options
-        )
+//         const result = await Model.findByIdAndUpdate(
+//             id, updatedData, options
+//         )
 
-        res.send(result)
-    }
-    catch (error) {
-        res.status(400).json({ message: error.message })
-    }
-})
+//         res.send(result)
+//     }
+//     catch (error) {
+//         res.status(400).json({ message: error.message })
+//     }
+// })
 
-//Delete by ID Method
-router.delete('/delete/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const data = await Model.findByIdAndDelete(id)
-        res.send(`Document with ${data.name} has been deleted..`)
-    }
-    catch (error) {
-        res.status(400).json({ message: error.message })
-    }
-})
+// //Delete by ID Method
+// router.delete('/delete/:id', async (req, res) => {
+//     try {
+//         const id = req.params.id;
+//         const data = await Model.findByIdAndDelete(id)
+//         res.send(`Document with ${data.name} has been deleted..`)
+//     }
+//     catch (error) {
+//         res.status(400).json({ message: error.message })
+//     }
+// })
