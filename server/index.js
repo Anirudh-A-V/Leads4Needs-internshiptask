a0da1995a53dc890ae7d20d42510c6dbb3962fd9
Anirudh A V <anirudh.av02@gmail.com>
2022-11-10 10:56:55 +0530

    /api added

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/', routes)
+app.use('/api', routes)
 
 // app.use(bodyParser.json({ limit: '30mb', extended: true }));
 // app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
