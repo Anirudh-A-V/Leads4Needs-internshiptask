5fba575d65f4d8aa16b406f71597e9c00b013496
Anirudh A V <anirudh.av02@gmail.com>
2022-11-10 12:49:36 +0530

    cors Options added

--- server/index.js
+++ server/index.js
@@ -12,9 +12,15 @@ app.use(express.json());
 
 app.use('/api', routes)
 
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ }
+
 // app.use(bodyParser.json({ limit: '30mb', extended: true }));
 // app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
-app.use(cors('*'));
+app.use(cors(corsOptions));
 
 dotenv.config();
 
