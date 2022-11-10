56f204305324d82253376b618dd5b93879e5147d
Anirudh A V <anirudh.av02@gmail.com>
2022-11-10 07:08:53 +0530

    API setup successful

--- server/index.js
+++ server/index.js
@@ -2,13 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+const routes = require('./routes/routes');
+
 const app = express();
 
 app.use(express.json());
-
-app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`)
-})
+app.use('/api', routes)
 
 dotenv.config();
 
@@ -20,3 +19,4 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
+
---
+++ server/models/model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const dataSchema = new mongoose.Schema({
+    task: {
+        required: true,
+        type: String
+    },
+    detail: {
+        required: true,
+        type: String
+    }
+})
+
+module.exports = mongoose.model('Data', dataSchema)
