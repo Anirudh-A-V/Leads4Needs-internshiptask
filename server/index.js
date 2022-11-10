d0a8d49dc8f19833434126847b555cc7faace3ad
Anirudh A V <anirudh.av02@gmail.com>
2022-11-10 10:29:52 +0530

    API completed

--- server/index.js
+++ server/index.js
@@ -20,9 +20,11 @@ dotenv.config();
 const CONNECTION_URL = process.env.MONGO_CONNECTION_URL;
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
+// Export the Express API
+module.exports = app;
